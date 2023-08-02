3707427c8c0454d3f1cd48376ca3d7231664811b
Abdelrahman Hegab <prenshegab@gmail.com>
2023-08-02 06:12:19 +0300

    feat/base file structure for the project

--- Shopping-List/src/app/app.module.ts
+++ Shopping-List/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './components/item/item.component';
+import { AddItemModalComponent } from './components/add-item-modal/add-item-modal.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    AddItemModalComponent,
+    MenuComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
---
+++ Shopping-List/src/app/services/interfaces/ItemModel.ts
@@ -0,0 +1,8 @@
+export interface ItemModel {
+    name: string;
+    photo: string;
+    price: number;
+    servingSize: string;
+    amountInStock: number;
+    amountInCart: number;
+}
