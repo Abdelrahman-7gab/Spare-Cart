327c37150306418215ebe6c4d55fe9309142f8c1
Abdelrahman Hegab <prenshegab@gmail.com>
2023-08-04 03:45:14 +0300

    feat/Full Responsive design css changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartInfoModel } from 'src/app/interfaces/ItemModel';
+import { ProductsService } from 'src/app/services/products.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -6,5 +11,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  cartInfo$: Observable<CartInfoModel>;
+
+  constructor(private productsService: ProductsService,private router: Router, private route: ActivatedRoute) {
+    this.cartInfo$ = this.productsService.getCartInfo$();
+   }
 
 }
